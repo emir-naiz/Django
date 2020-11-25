bb2f2fde5c00f00cef2986cc82d6accf04276a70
erjan <johnydepp2002@mail.ru>
2020-11-25 12:50:44 +0600

    new

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    models.ImageField()
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
